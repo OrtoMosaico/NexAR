e2f1511b53dd31e741ee8373de6c719e8c12e266
Damon <parasuscripcionescr@gmail.com>
2025-02-24 20:58:13 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,24 @@
- 
+// Importar las funciones necesarias de Firebase
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBKzibLWo4MNweXiwr6QoudB9sFb7CeH2w",
+  authDomain: "prueba-2-dc1d2.firebaseapp.com",
+  projectId: "prueba-2-dc1d2",
+  storageBucket: "prueba-2-dc1d2.appspot.com",
+  messagingSenderId: "603232032392",
+  appId: "1:603232032392:web:575f16d8e613348399313d",
+  measurementId: "G-J4QBH6WF5L"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportar servicios de Firebase
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);  
